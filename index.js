0c48e6cdbbcd19806f8e383a22f779f8c01b9b16
Rossev Gleb <rossevgleb@gmail.com>
2019-01-30 23:21:23 +0300

    Added roll1d100

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const Telegraf = require("telegraf");
 const Telegram = require("telegraf/telegram");
+const roll1d100 = require("./roll");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const telegram = new Telegram(process.env.BOT_TOKEN);
@@ -13,6 +14,49 @@ bot.help(ctx => {
 
 bot.command("cadia", ctx => {
   ctx.reply("Кадия пала, как и падет Империум!");
+});
+
+bot.command("roll1d100", ctx => {
+  const result = roll1d100();
+  if (result > 0 && result <= 10) {
+    ctx.reply(
+      `Твои шансы на провал ничтожно малы (${result}%), празднуй свою победу, раб трупа на троне...`
+    );
+  } else if (result > 10 && result <= 20) {
+    ctx.reply(
+      `Впечатляюще, сметный (${result}%), еще немного и мои ты почти догонишь нурглинга в своем изяществе.`
+    );
+  } else if (result > 20 && result <= 30) {
+    ctx.reply(
+      `Да, старайся впечатлить темных богов (${result}%), ты же так желаешь их благословеня`
+    );
+  } else if (result > 30 && result <= 40) {
+    ctx.reply(
+      `Ничтожная пешка (${result}%), наслаждайся мнимой свободой, пока...`
+    );
+  } else if (result > 40 && result <= 50) {
+    ctx.reply(`Бесполезно убегать, (${result}%) ты не сможешь бегать вечно`);
+  } else if (result > 50 && result <= 60) {
+    ctx.reply(
+      `Ты меня разочаровал(${result}%), я думал ты хоть на что-то способен.`
+    );
+  } else if (result > 60 && result <= 70) {
+    ctx.reply(
+      `Заурядно (${result}%), где-то между газами нурглита и клешней демонетки...`
+    );
+  } else if (result > 70 && result <= 80) {
+    ctx.reply(
+      `Сколько раз тебе нужно умереть что бы ты принял свою судьбу (${result}%)?`
+    );
+  } else if (result > 80 && result <= 90) {
+    ctx.reply(`Дразнишь богов? Ну что же, они тебя тоже (${result}%).`);
+  } else if (result > 90 && result < 100) {
+    ctx.reply(`А ты любишь играть со смертью, как я посмотрю (${result}%).`);
+  } else if (result === 100) {
+    ctx.reply(
+      `Теперь тебе уже ничего не поможет (${result}%). Хоть умри достойно, насекомое `
+    );
+  }
 });
 
 bot.hears("пидора ответ", ctx =>
---
+++ roll.js
@@ -0,0 +1,3 @@
+module.exports = function roll1d100() {
+  return Math.floor(Math.random() * 100);
+};
