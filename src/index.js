57c21778d5b54b846b2ecc9e8afb0a16f470ff0a
Rossev Gleb <rossevgleb@gmail.com>
2019-02-15 23:40:17 +0300

    Message to players added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const Telegraf = require("telegraf");
 const http = require("http");
 const mongoose = require("mongoose");
+const CronJob = require("cron").CronJob;
 
 const cadia = require("./controllers/cadia");
 const roll = require("./controllers/rolls/roll");
@@ -22,10 +23,7 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.start(ctx => ctx.reply("Чего ты желаешь, смертный?"));
 
 mongoose
-  .connect(
-    process.env.DB_URL,
-    { useNewUrlParser: true }
-  )
+  .connect(process.env.DB_URL, { useNewUrlParser: true })
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
@@ -44,11 +42,10 @@ bot.command("roll1d20", roll1d20);
 bot.command("roll1d66", roll1d66);
 bot.command("roll1d100", roll1d100);
 
-bot.hears("пидора ответ", ctx =>
-  ctx.reply(
-    "Считаешь это смешным? Посмотрим как ты посмеешься когда твое очко будет шире очка ужаса..."
-  )
-);
+bot.hears("нет", ctx => ctx.reply("Слаанешита ответ!"));
+
+bot.hears("Нет", ctx => ctx.reply("Слаанешита ответ!"));
+
 bot.hears("Хочу павер", ctx => ctx.reply("Вжух и ты демонпринц"));
 
 bot.on("message", ctx => {
@@ -56,6 +53,15 @@ bot.on("message", ctx => {
   const { text, date } = ctx.message;
   console.log({ first_name, last_name, username, text, date });
 });
+
+const job = new CronJob("00 29 14 15 1 *", function() {
+  bot.telegram.sendMessage(
+    -1001375734236,
+    "У меня есть для вас сообщение от генерала Анкая: “Вы моя элита, вы мой щит и мой меч.Вы были избраны для этого задания самим верховным правителем Абаддоном и он лично поручил мне распорядиться вами по достоинству.Вам предстоит нелегкий путь перевоплощений, обманов и кознь, но вы справитесь, вы были созданы для этого.Вам нужно убить жреца кабала Путракса и привести мне эту ведьму — Асгорху.Путь ваш будет невыносимо сложен, а в прочем вы и сами знаете.P.S. Гидра Доминатус!”"
+  );
+});
+
+job.start();
 
 bot.launch();
 http.createServer(resp).listen(process.env.PORT || 3000, err => {
