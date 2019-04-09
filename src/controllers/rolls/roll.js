d4450e29676fe191c2aa4a2bc9bc299008ef4d48
Rossev Gleb <rossevgleb@gmail.com>
2019-04-09 11:04:53 +0300

    Bux fix: roll overflow

--- src/controllers/rolls/roll.js
+++ src/controllers/rolls/roll.js
@@ -1,28 +1,32 @@
-const { roll } = require("./rollClassic");
+const {roll} = require ('./rollClassic');
 
 const rollCommand = ctx => {
   try {
-    const diceRoll = ctx.message.text.trim();
-    const rollRequest = diceRoll.split(" ")[1];
-    if (roll.validate(rollRequest)) {
-      const result = roll.roll(rollRequest);
-      return ctx.reply(`Твои кости показывают: ${result.rolled
-        .map(elem => "(" + elem + ")")
-        .toString()
-        .replace(/,/g, " ")}.\nВ итоге: ${result.result}
+    const diceRoll = ctx.message.text.trim ();
+    const rollRequest = diceRoll.split (' ')[1];
+    if (roll.validate (rollRequest)) {
+      console.log (rollRequest);
+      const quantity = rollRequest.split ('d')[0];
+      if (quantity > 100) {
+        return ctx.reply ('Слишком много просишь, ничтожество');
+      }
+      const result = roll.roll (rollRequest);
+
+      return ctx.reply (`Твои кости показывают: ${result.rolled
+        .map (elem => '(' + elem + ')')
+        .toString ()
+        .replace (/,/g, ' ')}.\nВ итоге: ${result.result}
         `);
     }
 
-    ctx.reply("Твой интеллект не позволяет тебе написать просьбу правильно?");
+    ctx.reply ('Твой интеллект не позволяет тебе написать просьбу правильно?');
 
-    ctx.reply(
-      `Пиши в формате — /roll "число кубов"d"число граней" (/roll 1d100, например), а не ${
-        ctx.message.text
-      }`
+    ctx.reply (
+      `Пиши в формате — /roll "число кубов"d"число граней" (/roll 1d100, например), а не ${ctx.message.text}`
     );
   } catch (error) {
-    console.error(error);
-    return new Error(error);
+    console.error (error);
+    return new Error (error);
   }
 };
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-require("dotenv").config();
-const Telegraf = require("telegraf");
-const http = require("http");
-const mongoose = require("mongoose");
+require ('dotenv').config ();
+const Telegraf = require ('telegraf');
+const http = require ('http');
+const mongoose = require ('mongoose');
 
-const cadia = require("./controllers/cadia");
-const roll = require("./controllers/rolls/roll");
+const cadia = require ('./controllers/cadia');
+const roll = require ('./controllers/rolls/roll');
 const {
   roll1d4,
   roll1d6,
@@ -13,57 +13,64 @@ const {
   roll1d12,
   roll1d20,
   roll1d66,
-  roll1d100
-} = require("./controllers/rolls/rollClassic");
-const resp = require("./response");
+  roll1d100,
+} = require ('./controllers/rolls/rollClassic');
+const resp = require ('./response');
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const bot = new Telegraf (process.env.BOT_TOKEN);
 
-bot.start(ctx => ctx.reply("Чего ты желаешь, смертный?"));
+bot.start (ctx => ctx.reply ('Чего ты желаешь, смертный?'));
 
 mongoose
-  .connect(process.env.DB_URL, { useNewUrlParser: true })
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
+  .connect (process.env.DB_URL, {useNewUrlParser: true})
+  .then (() => console.log ('MongoDB Connected'))
+  .catch (err => console.log (err));
 
-bot.help(ctx => {
-  ctx.reply("Я Абаддон Разоритель. Чего ты хочешь жалкий смертный? ");
+bot.help (ctx => {
+  ctx.reply ('Я Абаддон Разоритель. Чего ты хочешь жалкий смертный? ');
 });
 
-bot.command("cadia", cadia);
-bot.command("roll", roll);
-bot.command("roll1d4", roll1d4);
-bot.command("roll1d6", roll1d6);
-bot.command("roll1d8", roll1d8);
-bot.command("roll1d10", roll1d10);
-bot.command("roll1d12", roll1d12);
-bot.command("roll1d20", roll1d20);
-bot.command("roll1d66", roll1d66);
-bot.command("roll1d100", roll1d100);
+bot.command ('cadia', cadia);
+bot.command ('roll', roll);
+bot.command ('roll1d4', roll1d4);
+bot.command ('roll1d6', roll1d6);
+bot.command ('roll1d8', roll1d8);
+bot.command ('roll1d10', roll1d10);
+bot.command ('roll1d12', roll1d12);
+bot.command ('roll1d20', roll1d20);
+bot.command ('roll1d66', roll1d66);
+bot.command ('roll1d100', roll1d100);
 
-bot.on("message", ctx => {
-  const { first_name, last_name, username } = ctx.message.from;
-  const { text, date } = ctx.message;
-  console.log({ first_name, last_name, username, text, date, chatId: ctx.message.chat.id });
+bot.on ('message', ctx => {
+  const {first_name, last_name, username} = ctx.message.from;
+  const {text, date} = ctx.message;
+  console.log ({
+    first_name,
+    last_name,
+    username,
+    text,
+    date,
+    chatId: ctx.message.chat.id,
+  });
   if (ctx.message.chat.id === 186151380) {
-    const duobleDots = ctx.message.text.split("")[0]
-    if (duobleDots === ":") {
-      const re_text = ctx.message.text.slice(1);
-      ctx.telegram.sendMessage(process.env.GROUP_CHAT_ID, re_text);
-    };
+    const duobleDots = ctx.message.text.split ('')[0];
+    if (duobleDots === ':') {
+      const re_text = ctx.message.text.slice (1);
+      ctx.telegram.sendMessage (process.env.GROUP_CHAT_ID, re_text);
+    }
     // ctx.telegram.sendMessage(process.env.GROUP_CHAT_ID, ctx.message.text);
   }
 });
 
-bot.launch();
+bot.launch ();
 
-http.createServer(resp).listen(process.env.PORT || 3000, err => {
+http.createServer (resp).listen (process.env.PORT || 3000, err => {
   if (err) {
-    return console.error(err);
+    return console.error (err);
   }
-  console.log(`server is listening on ${process.env.PORT}`);
+  console.log (`server is listening on ${process.env.PORT}`);
 });
 
-setInterval(function () {
-  http.get("http://warhammer-log-bot.herokuapp.com");
+setInterval (function () {
+  http.get ('http://warhammer-log-bot.herokuapp.com');
 }, 300000);
